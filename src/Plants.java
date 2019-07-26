97aac980667ff5d9d1d6901339b94e505ddb57f0
greenjellyace <52971852+greenjellyace@users.noreply.github.com>
2019-07-26 13:32:09 -0600

    created constructors, still need revising

--- src/Plants.java
+++ src/Plants.java
@@ -3,6 +3,22 @@ public class Plants extends GameCharacter{
 	private int price;
 	private int row;
 	private int column;
+	
+	//not final, too many parameters, PLEASE EDIT, perhaps use an interface if possible
+	public Plants(String aName, int aHealth, int anAttack, int aPrice, int aRow, int  aColumn) {
+		super(aName, aHealth, anAttack);
+		this.price = aPrice;
+		this.row = aRow; //add if statements
+		this.column = aColumn; //add if statements
+	}
+	
+	public Plants(Plants aPlant) {
+		super(aPlant);
+		this.price = aPlant.price;
+		this.row = aPlant.price;
+		this.column = aPlant.price;
+	}
+	
 	public String getLocation(){
 		return "(" + row + ", " + column + ")";
 	}
@@ -16,7 +32,7 @@ public class Plants extends GameCharacter{
 		this.price = price;
 	}
 	public int getPrice() {
-		return price;
+		return this.price;
 	}
 	public int getRow() {
 		return row;
