f89879d03bcbcc2cfa96d48e7edc27fd6a0ee5d6
Luisav1 <52978048+Luisav1@users.noreply.github.com>
2019-07-26 19:06:54 -0600

    made it checkered

--- src/GardenScene.java
+++ src/GardenScene.java
@@ -1,26 +1,5 @@
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.*;
 
-public class GardenScene extends BaseScene {
-	
-	private Button[] gardenslots;
-	private final int MAXSLOTS = 45;
-	
-	//constructor, pass Session inst variable to parent
-	public GardenScene(Session aSession) {
-		super(aSession);
-	}
-
-	//override abstract parent
-	@Override
-	public void setup() {
-		StackPane root = new StackPane();
-		Scene scene = new Scene(root, 1050, 720);
-		
-		//set up gridpane
-		GridPane firstStack = new GridPane();import javafx.geometry.Insets;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
@@ -28,12 +7,12 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.*;
 
 public class GardenScene extends BaseScene {
-	
+
 	private Button[] gardenslots;
 	private final int MAXSLOTS = 45;
 	private final int LENGTH = 1220;
 	private final int WIDTH = 720;
-	
+
 	//constructor, pass Session inst variable to parent
 	public GardenScene(Session aSession) {
 		super(aSession);
@@ -47,41 +26,41 @@ public class GardenScene extends BaseScene {
 
 		//Initialize game
 		Player player = new Player();
-		Game game = new Game(player);		
-		
+		Game game = new Game(player);
+
 		root.getChildren().add(gardenButtons(game.getPlayer()));
-		
+
 		setScene(scene);
 		display();
-		
+
 	}
-	
+
 	public Node gardenButtons(Player aPlayer) {
-		
+
 		VBox root = new VBox();
 
 //		for (int column = 0; column = )
 		Button peaShooter = new Button("Peashooter");
 		Button cherryBomb = new Button("Cherrybomb");
 		Button frozenPeaShooter = new Button("Frozen Peashooter");
-		
+
 		//setup plants buttons in first row of layout
 		HBox box = new HBox();
 		box.getChildren().addAll(peaShooter, cherryBomb,frozenPeaShooter);
-		
+
 		//create empty button array for button
 		gardenslots = new Button[MAXSLOTS];	//number of elements must be stated
 		for (int i = 0; i < MAXSLOTS; ++i) {
 			gardenslots[i] = new Button("Button"+(i+1));	//label on buttons
 			gardenslots[i].setPrefSize(100,  106);	//set button size
 		}
-		
+
 		//set up gridpane as second row of layout
 		GridPane grid = new GridPane();
 		grid.setPadding(new Insets(0, 22, 32, 0));
 //		grid.setGridLinesVisible(true);
 		grid.setAlignment(Pos.BOTTOM_RIGHT);
-		
+
 		//create and add every button into array
 		int button = 0;
 		for (int row = 0; row < 5; ++row) {
@@ -90,11 +69,11 @@ public class GardenScene extends BaseScene {
 				++button;
 			}
 		}
-		
+
 		//add firstrow (plantwidgets) and secondrow (gardenslots) to root
 		root.getChildren().add(box);
-		root.getChildren().add(grid);	
-		
+		root.getChildren().add(grid);
+
 		return root;
 	}
 
